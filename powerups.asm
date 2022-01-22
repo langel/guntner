524086a53b46a7e025ce87106712eab314eefcb9
langel <knoxburry@gmail.com>
2022-01-21 19:18:01 -0500

    player can juggle powerups

--- powerups.asm
+++ powerups.asm
@@ -26,9 +26,11 @@ powerup_spawn:
         ldy enemy_ram_oam,x
         sta $0200,y
         
-powerup_init_velocity:
+powerup_init_velocity_and_hp:
 	lda #$7f
         sta enemy_ram_pc,x
+        lda #$02
+        sta enemy_ram_hp,x
         rts
         
 powerup_from_starglasses:
@@ -39,7 +41,7 @@ powerup_from_starglasses:
         sta enemy_ram_x,x
         lda $0200,y
         sta enemy_ram_y,x
-       	jsr powerup_init_velocity
+       	jsr powerup_init_velocity_and_hp
 	rts
       
         
@@ -63,24 +65,97 @@ powerups_cycle: subroutine
         adc enemy_ram_pc,x
         bcc .not1
         dec enemy_ram_x,x
+        bne .no_hp1 ; don't increase hp
+        inc enemy_ram_hp,x
+.no_hp1
         clc
 .not1        
         adc enemy_ram_pc,x
         bcc .not2
         dec enemy_ram_x,x
+        bne .no_hp2 ; don't increase hp
+        inc enemy_ram_hp,x
+.no_hp2
         clc
 .not2      
         adc enemy_ram_pc,x
         bcc .not3
         dec enemy_ram_x,x
+        bne .no_hp3 ; don't increase hp
+        inc enemy_ram_hp,x
+.no_hp3
         clc
 .not3      
         sta enemy_ram_ac,x
         dec enemy_ram_pc,x
         jmp .move_done
 .move_right
+	cmp #$00
+        beq .right_max
+        cmp #$72
+        bcc .right_speed1
+        dec enemy_ram_pc,x
+        jmp .move_done
+.right_speed1
+	cmp #$62
+        bcc .right_speed2
 	inc enemy_ram_x,x
+        bne .no_hp6
+        dec enemy_ram_hp,x
+.no_hp6
+        dec enemy_ram_pc,x
+        jmp .move_done
+.right_speed2
+	cmp #$50
+        bcc .right_speed3
+	inc enemy_ram_x,x
+        bne .no_hp7
+        dec enemy_ram_hp,x
+.no_hp7
+	inc enemy_ram_x,x
+        bne .no_hp8
+        dec enemy_ram_hp,x
+.no_hp8
+        dec enemy_ram_pc,x
+        jmp .move_done
+.right_speed3
+	cmp #$3a
+        bcc .right_set_max
+	inc enemy_ram_x,x
+        bne .no_hp9
+        dec enemy_ram_hp,x
+.no_hp9
+	inc enemy_ram_x,x
+        bne .no_hpa
+        dec enemy_ram_hp,x
+.no_hpa
+	inc enemy_ram_x,x
+        bne .no_hpb
+        dec enemy_ram_hp,x
+.no_hpb
+        dec enemy_ram_pc,x
+        jmp .move_done
+.right_set_max
+	lda #$00
+        sta enemy_ram_pc,x
+.right_max
+	inc enemy_ram_x,x
+	inc enemy_ram_x,x
+	inc enemy_ram_x,x
+	inc enemy_ram_x,x
+	lda enemy_ram_x,x
+        cmp #$fc
+        bcc .move_done
+        dec enemy_ram_hp,x
 .move_done
+.despawn_check
+	lda enemy_ram_hp,x
+        cmp #$00
+        bne .update_oam_data
+ 	;; XXX needs its own sfx
+        jsr apu_trigger_enemy_death
+        jsr enemy_death
+.update_oam_data
         lda enemy_ram_x,x
 	sta collision_0_x
         sta $0203,y
@@ -89,8 +164,9 @@ powerups_cycle: subroutine
 	sta collision_0_y
         sta $0200,y
         ; dim for collision
-        lda #$08
+        lda #$06
         sta collision_0_w
+        lda #$08
         sta collision_0_h
         jsr player_bullet_collision_handler
 	cmp #$00
