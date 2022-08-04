4bec1454a7bab153386ad1dfc5c184af31b1ce32
langel <knoxburry@gmail.com>
2022-08-04 12:00:39 -0400

    muya working as intended

--- enemy_muya.asm
+++ enemy_muya.asm
@@ -5,9 +5,8 @@ muya_spawn: subroutine
 	jsr get_oam_offset_from_slot_offset
         jsr get_next_random
         lsr
-        adc #$70
-      	sta oam_ram_x,y
-      	sta oam_ram_x+4,y
+        adc #$5c
+        sta enemy_ram_x,x
         lda #muya_spawn_y
         sta oam_ram_y,y
    	rts
@@ -20,7 +19,7 @@ muya_cycle: subroutine
         sta collision_0_w
         lda #$10
         sta collision_0_h
-        ;jsr enemy_handle_damage_and_death
+        jsr enemy_handle_damage_and_death
         
 	; sprite
         lda #$58
@@ -28,17 +27,14 @@ muya_cycle: subroutine
         lda #$68
         sta oam_ram_spr+4,y
         
-        ; reset check
-        lda enemy_ram_ac,x
-        cmp #$40
-        bcc .dont_reset
-        lda #$00
-        sta enemy_ram_ac
-        lda #muya_spawn_y
-        sta oam_ram_y,y
-.dont_reset
+        ; x pos
+        lda enemy_ram_x,x
+      	sta oam_ram_x,y
+      	sta oam_ram_x+4,y
         
         ; y pos
+        ; XXX could reuse crossbones movement code here
+        inc enemy_ram_ac,x
         lda enemy_ram_ac,x
         lsr
         lsr
@@ -47,18 +43,25 @@ muya_cycle: subroutine
         lda oam_ram_y,y
         sec
         sbc temp00
+        cmp sprite_0_y
+        bcs .reset_y_pos
         sta oam_ram_y,y
         clc
         adc #$08
         sta oam_ram_y+4,y
+        bne .reset_skip
+.reset_y_pos
+        lda #$00
+        sta enemy_ram_ac,x
+        lda #muya_spawn_y
+        sta oam_ram_y,y
+.reset_skip
         
         ; palette
         lda #$01
         jsr enemy_set_palette
         sta oam_ram_att+4,y
-        
-        inc enemy_ram_ac,x
-        
-        
+              
 	jmp update_enemies_handler_next
+        
         
--- state_sandbox.asm
+++ state_sandbox.asm
@@ -26,7 +26,7 @@ sandbox_init: subroutine
         
         ldx #$b8
         lda #boss_swordtner_id
-        ;jsr enemy_spawn_delegator
+        jsr enemy_spawn_delegator
         
         ; SWORDTNER palettes
         ldx #69
@@ -62,15 +62,6 @@ sandbox_init: subroutine
         lda #muya_id
         jsr enemy_spawn_delegator
         
-	jsr get_enemy_slot_2_sprite
-        tax
-        lda #muya_id
-        jsr enemy_spawn_delegator
-        
-	jsr get_enemy_slot_2_sprite
-        tax
-        lda #muya_id
-        jsr enemy_spawn_delegator
         
         
         
